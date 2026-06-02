8eb6faadfb228e71d8b5d1858c4b3cfa4c0706da
tiktokbaru3377-ship-it <tiktokbaru3377@gmail.com>
2026-06-03 06:45:44 +0700

    Create useToastStore.js

---
+++ src/store/useToastStore.js
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+export const useToastStore = create((set) => ({
+  toasts: [],
+  addToast: (message, type = 'info') => {
+    const id = Math.random().toString(36).substring(2, 9);
+    set((state) => ({ toasts: [...state.toasts, { id, message, type }] }));
+    
+    // Otomatis musnahkan elemen alert setelah berumur 5 detik
+    setTimeout(() => {
+      set((state) => ({ toasts: state.toasts.filter((t) => t.id !== id) }));
+    }, 5000);
+  },
+  removeToast: (id) => set((state) => ({ toasts: state.toasts.filter((t) => t.id !== id) }))
+}));
