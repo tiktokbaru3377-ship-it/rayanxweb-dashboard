cf100befcbb624c8b35f3bdb532ca144c2bab94f
tiktokbaru3377-ship-it <tiktokbaru3377@gmail.com>
2026-06-03 09:26:25 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
 
-// PERBAIKAN JALUR: Karena main.jsx dan index.css sama-sama berada di dalam folder src
-import './index.css'; 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Struktur render resmi dan stabil untuk React 18
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
